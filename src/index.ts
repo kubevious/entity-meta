3a574632e1a805393187af3daf0893c202d05061
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-09-28 14:22:58 -0700

    exposed rn parser

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export { PropsKind } from './entities/props-kind'
 export { PROPS_TOOLTIPS } from './prop-tooltips';
 export { DIAGRAM_LABELS } from './diagram-labels';
 export { DIAGRAM_ICONS } from './diagram-icons';
+
+export { RnInfo, parseDn, makeDn } from './dn-utils'
