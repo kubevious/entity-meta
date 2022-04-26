996edfd29e22193f7ab204bc3d0178b7469a79f8
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-26 16:18:33 -0700

    updates to pod health props

--- src/props-config/pods-versions-health.ts
+++ src/props-config/pods-versions-health.ts
@@ -21,4 +21,30 @@ export interface PodHealthInfo
     dn: string;
     date: string;
     phase: string;
+    runStage: string;
+    conditions: PodHealthCondition[];
+}
+
+export interface PodHealthCondition
+{
+    type: string;
+    state: boolean;
+}
+
+
+export enum PodPhase {
+    Pending = "Pending",
+    Running = "Running",
+    Succeeded = "Succeeded",
+    Failed = "Failed",
+    Unknown = "Unknown",
+}
+
+export enum PodRunStage {
+    Scheduling = "Scheduling",
+    Initializing = "Initializing",
+    WaitingContainers = "WaitingContainers",
+    WaitingConditions = "WaitingConditions",
+    WaitingReady = "WaitingReady",
+    Ready = "Ready"
 }
