071beb878c05bee0b0ca0e96238b1ccedd8c862c
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-27 22:01:17 -0700

    updates to props kinds

--- src/entities/props-kind.ts
+++ src/entities/props-kind.ts
@@ -14,6 +14,8 @@ export enum PropsKind
 
     ruleAssistant = 'rule-assistant',
 
-    healthTable = 'health-table',
+    healthTable = 'health-table', // To Retire
+    workloadsHealth = 'workloads-health',
+    podHealth = 'pod-health',
     podsVersionsHealth = 'pods-versions-health',
 }
